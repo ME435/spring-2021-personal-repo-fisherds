fe4411da564e47346c722c7888b0817da8488191
David Fisher <fisherds@rose-hulman.edu>
2021-03-22 14:52:12 -0400

    create 3 copies of blink.js

---
+++ Node/GPIO_day2/using_onoff/blink.js
@@ -0,0 +1,24 @@
+// Imports
+function msleep(n) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+ }
+ 
+ function sleep(n) {
+    msleep(n * 1000);
+ }
+
+ function main() {
+     console.log("Ready");
+     // TODO: Setup GPIO 14 as an OUTPUT
+
+     for (let k = 0; k < 3; k++) {
+         console.log("LED On");
+         sleep(1);
+
+         console.log("LED OFF");
+         sleep(1);
+     }
+
+ }
+
+ main();
