83c0e189efbf03a0b47323c908d2f0d995e80898
David Fisher <fisherds@rose-hulman.edu>
2021-03-23 09:09:25 -0400

    learning pigpio

--- Node/GPIO_day2/using_pigpio/blink.js
+++ Node/GPIO_day2/using_pigpio/blink.js
@@ -1,4 +1,5 @@
-// Imports
+const Gpio = require('pigpio').Gpio;
+
 function msleep(n) {
     Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
  }
@@ -8,14 +9,18 @@ function msleep(n) {
  }
 
  function main() {
-     console.log("Ready");
+     console.log("Ready using Pi GPIO");
      // TODO: Setup GPIO 14 as an OUTPUT
+     const led = new Gpio(14, {mode: Gpio.OUTPUT});
+
 
      for (let k = 0; k < 3; k++) {
          console.log("LED On");
+         led.digitalWrite(1);
          sleep(1);
 
          console.log("LED OFF");
+         led.digitalWrite(0);
          sleep(1);
      }
 
