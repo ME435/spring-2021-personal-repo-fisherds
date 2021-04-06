d44b69408b0182b64b11b666f78a2514deca3c69
David Fisher <fisherds@rose-hulman.edu>
2021-04-06 13:04:11 -0400

    copy in the test starter code

---
+++ Node/TankDriveServer/testDriveSystem.js
@@ -0,0 +1,41 @@
+const prompt = require('prompt-sync')({sigint: true});
+// # -------------------------------------------------------------------------
+// # TODO: import rosebot
+//         const rosebot = require("./rosebot");
+// # -------------------------------------------------------------------------
+
+function msleep(n) {
+  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
+function sleep(n) {
+  msleep(n*1000);
+}
+
+function main() {
+  console.log('--------------------------------------------------')
+  console.log('Testing the  DRIVE SYSTEM  of a robot')
+  console.log('--------------------------------------------------')
+    // # -------------------------------------------------------------------------
+    // # TODO: Create a robot.
+    // # -------------------------------------------------------------------------
+
+  while (true) {
+    console.log("Wheel speeds should be integers between -100 and 100.")
+    console.log("Enter values of 0 for both to exit.")
+    let leftWheelSpeed = parseInt(prompt('Enter an integer for left wheel speed: '));
+    let rightWheelSpeed = prompt('Enter an integer for right wheel speed: ');
+    rightWheelSpeed = parseInt(rightWheelSpeed);
+    if (leftWheelSpeed == 0 && rightWheelSpeed == 0) {
+      break;
+    }
+    prompt('Press the ENTER key when ready for the robot to start moving.');
+    // # -------------------------------------------------------------------------
+    // # TODO: Call the  go  method of the   driveSystem   of the robot,
+    // #   sending it the two wheel speeds.  Keep going (sleep) for 3 seconds.
+    // #   Then call the  stop  method of the   driveSystem   of the robot.
+    // # -------------------------------------------------------------------------
+
+  }
+}
+
+main();
