59477873a5ea7c2773c81aada1d9321d0210cc87
David Fisher <fisherds@rose-hulman.edu>
2021-04-07 09:53:57 -0400

    tank controller

--- Node/TankDriveServer/public/scripts/main.js
+++ Node/TankDriveServer/public/scripts/main.js
@@ -1,26 +1,35 @@
-/** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
-
-/** function and class syntax examples */
-rhit.functionName = function () {
-};
-
-rhit.ClassName = class {
+rhit.TankController = class {
 	constructor() {
+		const buttons = document.querySelectorAll(".driveButton");
+		const sliderInput = document.querySelector("#baseSpeed");
 
+		for (const button of buttons) {
+			button.onclick = (event) => {
+				const leftMultiplier = button.dataset.leftMultiplier;
+				const rightMultiplier = button.dataset.rightMultiplier;
+				const baseSpeed = sliderInput.value;
+				const leftWheelSpeed = baseSpeed * leftMultiplier;
+				const rightWheelSpeed = baseSpeed * rightMultiplier;
+				this.sendDriveCommand(leftWheelSpeed, rightWheelSpeed);
+			};
+		}
 	}
 
-	methodName() {
-
+	sendDriveCommand(leftWheelSpeed, rightWheelSpeed) {
+		console.log(`Sent these values to the server: ${leftWheelSpeed} ${rightWheelSpeed}`);
+		fetch(`api/motor/go/${leftWheelSpeed}/${rightWheelSpeed}`);
+	}
+	sendStop() {
+		fetch(`api/motor/stop`);
 	}
 }
 
 /* Main */
 rhit.main = function () {
 	console.log("Ready");
+	tankController = new rhit.TankController();
 };
 
 rhit.main();
