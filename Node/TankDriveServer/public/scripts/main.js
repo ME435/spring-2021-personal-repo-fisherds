c3624f3cecd0045f2730958857bd53641f4d26e3
David Fisher <fisherds@rose-hulman.edu>
2021-04-07 10:03:02 -0400

    special desktop mouse and key callbacks

--- Node/TankDriveServer/public/scripts/main.js
+++ Node/TankDriveServer/public/scripts/main.js
@@ -6,15 +6,56 @@ rhit.TankController = class {
 		const sliderInput = document.querySelector("#baseSpeed");
 
 		for (const button of buttons) {
-			button.onclick = (event) => {
+			// For mobile using onclick is GREAT and WORKS!
+
+			// button.onclick = (event) => {
+			// 	const leftMultiplier = button.dataset.leftMultiplier;
+			// 	const rightMultiplier = button.dataset.rightMultiplier;
+			// 	const baseSpeed = sliderInput.value;
+			// 	const leftWheelSpeed = baseSpeed * leftMultiplier;
+			// 	const rightWheelSpeed = baseSpeed * rightMultiplier;
+			// 	this.sendDriveCommand(leftWheelSpeed, rightWheelSpeed);
+			// };
+
+			// Desktop only
+			button.onmousedown = (event) => {
 				const leftMultiplier = button.dataset.leftMultiplier;
 				const rightMultiplier = button.dataset.rightMultiplier;
 				const baseSpeed = sliderInput.value;
 				const leftWheelSpeed = baseSpeed * leftMultiplier;
 				const rightWheelSpeed = baseSpeed * rightMultiplier;
 				this.sendDriveCommand(leftWheelSpeed, rightWheelSpeed);
-			};
+			}
+			button.onmouseup = (event) => {
+				this.sendStop();
+			}
+
+			document.onkeydown = (event) => {
+				// console.log(`Type: ${event.type}   Key: ${event.key}`);
+				this.handleKeyPress(event);
+			}
+			document.onkeyup = (event) => {
+				// console.log(`Type: ${event.type}   Key: ${event.key}`);
+				this.handleKeyPress(event);
+			}
+
 		}
+	}
+
+	handleKeyPress(event) {
+		if (event.type == "keydown") {
+			if (event.key == "ArrowUp") {
+				this.sendDriveCommand(100, 100);
+			} else if (event.key == "ArrowDown") {
+				this.sendDriveCommand(-100, -100);
+			} else if (event.key == "ArrowLeft") {
+				this.sendDriveCommand(-100, 100);
+			} else if (event.key == "ArrowRight") {
+				this.sendDriveCommand(100, -100);
+			}
+		} else if (event.type == "keyup") {
+			this.sendStop();
+		} 
 	}
 
 	sendDriveCommand(leftWheelSpeed, rightWheelSpeed) {
