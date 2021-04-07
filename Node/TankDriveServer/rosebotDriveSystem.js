bbdc91b9c4bcd2c855166a025e389cc36329907e
David Fisher <fisherds@rose-hulman.edu>
2021-04-07 08:33:41 -0400

    test the DriveSystem

--- Node/TankDriveServer/rosebotDriveSystem.js
+++ Node/TankDriveServer/rosebotDriveSystem.js
@@ -37,8 +37,32 @@ class Motor {
   }
 }
 
-// Test code area:
+class DriveSystem {
+  constructor() {
+    const Motor_A_EN = 4;
+    const Motor_B_EN = 17; // No hardware PWM!!!
+    const Motor_A_Pin1 = 14;
+    const Motor_A_Pin2 = 15;
+    const Motor_B_Pin1 = 27;
+    const Motor_B_Pin2 = 18;
+    this.leftMotor = new Motor(Motor_B_Pin1, Motor_B_Pin2, Motor_B_EN);
+    this.rightMotor = new Motor(Motor_A_Pin2, Motor_A_Pin1, Motor_A_EN);
+  }
 
+  go(leftSpeed, rightSpeed) {
+    // speeds are values from -100 to 100.
+    this.leftMotor.turnOn(leftSpeed);
+    this.rightMotor.turnOn(rightSpeed);
+  }
+
+  stop() {
+    this.leftMotor.turnOff();
+    this.rightMotor.turnOff();
+  }
+}
+
+
+// Test code area:
 function msleep(n) {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
@@ -46,11 +70,16 @@ function sleep(n) {
   msleep(n*1000);
 }
 
-const Motor_A_EN = 4;
-const Motor_B_EN = 17; // No hardware PWM!!!
-const Motor_A_Pin1 = 14;
-const Motor_A_Pin2 = 15;
-const Motor_B_Pin1 = 27;
-const Motor_B_Pin2 = 18;
-const leftMotor = new Motor(Motor_B_Pin1, Motor_B_Pin2, Motor_B_EN);
-const rightMotor = new Motor(Motor_A_Pin2, Motor_A_Pin1, Motor_A_EN);
+const driveSystem = new DriveSystem();
+driveSystem.go(60, 60);
+sleep(2);
+driveSystem.stop();
+sleep(1);
+driveSystem.go(-60, -60);
+sleep(2);
+driveSystem.stop();
+sleep(1);
+driveSystem.go(-60, 60);
+sleep(2);
+driveSystem.stop();
+
