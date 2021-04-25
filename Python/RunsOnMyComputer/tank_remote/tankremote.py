4a565421aed69a0312f39b500073e2ae998a12d9
David Fisher <fisherds@rose-hulman.edu>
2021-04-24 21:53:00 -0400

    progress on my tank remote

--- Python/RunsOnMyComputer/tank_remote/tankremote.py
+++ Python/RunsOnMyComputer/tank_remote/tankremote.py
@@ -10,19 +10,16 @@ class TankRemoteApp(MDApp):
 
         self.mqtt_client = mqtt_helper.MqttClient()
         self.mqtt_client.callback = self.mqtt_callback
-        self.mqtt_client.connect(subscription_topic_name="fisherds/messagesForComputer", 
+        self.mqtt_client.connect(subscription_topic_name="fisherds/messagesForPi", # TODO: Put this back to Computer! 
                                     publish_topic_name="fisherds/messagesForPi")
 
     def mqtt_callback(self, message_type, payload):
         print("MQTT message_type", message_type)
         print("MQTT payload", payload)
 
-        self.update_view()
-
-    def update_view(self):
-        print("TODO: Update the view as needed")
 
     def build(self):
+        self.theme_cls.primary_palette = "BlueGray"
         Window.size = (400, 600)
         return
 
