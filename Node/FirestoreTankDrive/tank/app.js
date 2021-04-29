5632ccd2ad191d2fd39c960967d5167b657975ab
David Fisher <fisherds@rose-hulman.edu>
2021-04-29 13:10:21 -0400

    finished Firestore tank drive

--- Node/FirestoreTankDrive/tank/app.js
+++ Node/FirestoreTankDrive/tank/app.js
@@ -1,11 +1,38 @@
 const admin = require('firebase-admin');
-
 const serviceAccount = require('./serviceAccountKey.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+const rosebot = require("./rosebot");
+const robot = new rosebot.RoseBot();
 
-const db = admin.firestore();
+// Make a ref to the document
+const ref = admin.firestore().collection("Commands").doc("command");
 
-console.log(db);
+// Listen for real time updates
+ref.onSnapshot((docSnapshot) => {
+  if (docSnapshot.exists) {
+    console.log(`Received doc snapshot:`, docSnapshot.data());
+    const message_type = docSnapshot.get("type");
+    let payload = docSnapshot.get("payload");
+    if (payload) {
+      payload = JSON.parse(payload);
+    }
+    handleMessage(message_type, payload);
+  } else {
+    console.log(`Document missing!`);  
+  }
+}, (err) => {
+  console.log(`Encountered error: ${err}`);
+});
+
+// Parse the response and DO IT!
+function handleMessage(message_type, payload) {
+  if (message_type == "motor/go") {
+    const leftWheelSpeed = payload[0];
+    const rightWheelSpeed = payload[1];
+    robot.driveSystem.go(leftWheelSpeed, rightWheelSpeed);
+  } else if (message_type == "motor/stop") {
+    robot.driveSystem.stop();
+  }
+}
