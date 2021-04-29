6d7f1c4025a9df6988ee85ad73c5f450760fe134
David Fisher <fisherds@rose-hulman.edu>
2021-04-29 08:28:34 -0400

    finished Firebase tank drive

--- Node/FirestoreTankDrive/tank/app.js
+++ Node/FirestoreTankDrive/tank/app.js
@@ -3,34 +3,38 @@ const serviceAccount = require('./serviceAccountKey.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
 const rosebot = require("./rosebot");
 const robot = new rosebot.RoseBot();
-https://fisherds-moviequoteusers.web.app/images/rose_logo.png
+
+function handleMessage(docSnapshot) {
+  const message_type = docSnapshot.get("type");
+  let payload = docSnapshot.get("payload");
+  if (payload) {
+    payload = JSON.parse(payload);
+  }
+
+  if (message_type == "motor/go") {
+    const leftWheelSpeed = payload[0];
+    const rightWheelSpeed = payload[1];
+    console.log(`Motor go at ${leftWheelSpeed} ${rightWheelSpeed}`);
+    robot.driveSystem.go(leftWheelSpeed, rightWheelSpeed);
+  } else if (message_type == "motor/stop") {
+    console.log("Motor stop");
+    robot.driveSystem.stop();
+  }
+}
+
+// Create a ref to the spot in the cloud.
 const db = admin.firestore();
 const ref = db.collection("Commands").doc("command");
-
 ref.onSnapshot((docSnapshot) => {
-  if (docSnapshot.exists) {
-    console.log(docSnapshot.data());
-
-    const messageType = docSnapshot.get("type");
-    let payload = docSnapshot.get("payload");
-    if (payload) {
-      payload = JSON.parse(payload);
+    if (docSnapshot.exists) {
+      console.log(`Data: `, docSnapshot.data());
+      handleMessage(docSnapshot);
+    } else {
+      console.log("Error: There is no document at this location");
     }
-
-    if (messageType == "motor/go") {
-      const leftWheelSpeed = payload[0];
-      const rightWheelSpeed = payload[1];
-      console.log("Motor go @", leftWheelSpeed, rightWheelSpeed);
-      robot.driveSystem.go(leftWheelSpeed, rightWheelSpeed);
-
-    } else if (messageType == "motor/stop") {
-      console.log("Motor Stop");
-      robot.driveSystem.stop();
-    }
-  }
-}, (err) => {
-  console.log(`Error: ${err}`)
-});
+  },
+  (err) => {
+    console.log(`Encountered error: ${err}`);
+  });
