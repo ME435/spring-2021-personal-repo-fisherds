44db00d7e7aeb5dc5986d1eaaa9bc00e15a5d211
David Fisher <fisherds@rose-hulman.edu>
2021-04-29 09:44:59 -0400

    reset final video (again)

--- Node/FirestoreTankDrive/tank/app.js
+++ Node/FirestoreTankDrive/tank/app.js
@@ -1,40 +1,11 @@
 const admin = require('firebase-admin');
+
 const serviceAccount = require('./serviceAccountKey.json');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-const rosebot = require("./rosebot");
-const robot = new rosebot.RoseBot();
 
-function handleMessage(docSnapshot) {
-  const message_type = docSnapshot.get("type");
-  let payload = docSnapshot.get("payload");
-  if (payload) {
-    payload = JSON.parse(payload);
-  }
-
-  if (message_type == "motor/go") {
-    const leftWheelSpeed = payload[0];
-    const rightWheelSpeed = payload[1];
-    console.log(`Motor go at ${leftWheelSpeed} ${rightWheelSpeed}`);
-    robot.driveSystem.go(leftWheelSpeed, rightWheelSpeed);
-  } else if (message_type == "motor/stop") {
-    console.log("Motor stop");
-    robot.driveSystem.stop();
-  }
-}
-
-// Create a ref to the spot in the cloud.
 const db = admin.firestore();
-const ref = db.collection("Commands").doc("command");
-ref.onSnapshot((docSnapshot) => {
-    if (docSnapshot.exists) {
-      console.log(`Data: `, docSnapshot.data());
-      handleMessage(docSnapshot);
-    } else {
-      console.log("Error: There is no document at this location");
-    }
-  },
-  (err) => {
-    console.log(`Encountered error: ${err}`);
-  });
+
+console.log(db);
